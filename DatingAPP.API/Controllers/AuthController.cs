fc1d0798150d40d1c0c88c64de3492a058a4bdcd
radu puspana <rpuspana@gmail.com>
2018-04-27 16:42:29 +0300

    Adding UserForLoginDto, generating an auth jwt token

--- DatingAPP.API/Controllers/AuthController.cs
+++ DatingAPP.API/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using DatingAPP.API.Dtos;
 using DatingAPP.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingAPP.API.Data
 {
@@ -51,6 +55,60 @@ namespace DatingAPP.API.Data
 
             // return a http response to the client, 201 Created
             return StatusCode(201);
+        }
+
+        [HttpPost("login")] // add another word to the URL aka api/auth/login
+        public async Task<IActionResult> Login([FromBody] UserForLoginDto userForLoginDto)
+        {
+            // login with username and password
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(),
+                                                 userForLoginDto.Password);
+
+            // check if it's a user in the response
+            if (userFromRepo == null)
+            {
+                // don't say if the user existed or not
+                return Unauthorized();
+            }
+
+            // generating the token that will send back to the end user
+
+            var tokenHandle = new JwtSecurityTokenHandler();
+
+            //super secret key =  key to sign the token encoded in bytes array
+            // like 7A 61 2B 41 77 59 42 2F 51 4F 79 32 50 2F 63 2F 77 2D
+            var key = System.Text.Encoding.ASCII.GetBytes("super secret key");
+
+            // token payload = describe token and what is going to be inside it
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+
+                // token secret
+                SigningCredentials = 
+                    new SigningCredentials(new SymmetricSecurityKey(key), 
+                                            SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            // create a JWT token
+            var token = tokenHandle.CreateToken(tokenDescriptor);
+
+            // Serialization = process of converting an object into a stream of bytes
+            //  in order to store the object or transmit it to memory, a database, or a file.
+            //  Its main purpose is to save the state of an object 
+            //  in order to be able to recreate it when needed.
+            // The reverse process is called deserialization.
+
+            // serialize the token
+            var tokenString = tokenHandle.WriteToken(token);
+
+            // pass token to the client
+            return Ok(new {tokenString});
         }
     }
 }
